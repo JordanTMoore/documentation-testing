330cb90bfa1c62cce2a77a3cf04f85f9a1573137
JORDAN T. MOORE <jtmoore@us.ibm.com>
2020-06-16 19:12:50 -0400

    Jurastic Pages Change Test

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome to v1.04!",
+        "excerpt":"In this update we explore the possibilities of analytics with IBM TRIRIGA Assistant   ToDo: get more info here  Release notes updates here  ","categories": ["ta","update"],
+        "tags": [],
+        "url": "http://localhost:4000/ta/update/2020/06/16/welcome-to-jekyll.html",
+        "teaser": null
+      }]
