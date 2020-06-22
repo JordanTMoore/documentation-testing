1c41362b23981ed4c98bc53832c0bed1f9be2cbc
JORDAN T. MOORE <jtmoore@us.ibm.com>
2020-06-22 16:51:05 -0400

    Nuke it all, see how we do

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Welcome to v1.04!",
-        "excerpt":"In this update we explore the possibilities of analytics with IBM TRIRIGA Assistant   ToDo: get more info here  Release notes updates here  ","categories": ["ta","update"],
+        "excerpt":"In this update we explore the possibilities of analytics with IBM TRIRIGA Assistant. With Version 1.0.4 we are introducing a new dashboard for TRIRIGA Administrators to help understand how the Assistant is being consumed by end users. This new reporting dashboard helps visualize usage statistics like daily concurrent users, or...","categories": ["ta","update"],
         "tags": [],
         "url": "http://localhost:4000/ta/update/2020/06/16/welcome-to-jekyll.html",
         "teaser": null
